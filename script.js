7bdbf6c18d4a8fb6d5bf99292e9e9c4a6fc32e58
heshamelmasry77 <heshamelmasry@outlook.com>
2017-08-24 22:02:58 +0200

    Adding some function to select the date.

--- script.js
+++ script.js
@@ -1,61 +1,63 @@
-(function () {
-    var nowYear = moment().format('YYYY');
-    // moment().format('MMMM Do YYYY, h:mm:ss a'); // August 24th 2017, 1:47:58 pm
-    var nowMonth = moment().format('MMMM');
-    var monthNames = ["January", "February", "March", "April", "May", "June",
-        "July", "August", "September", "October", "November", "December"];
-    var text = "";
+var nowYear = moment().format('YYYY');
+var monthNames = ["January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December"];
+var selectedYear = "";
+var month = "";
+var selectedMonth = "";
 
-    getMonth();
-    getYear();
-    getDays();
+getMonth();
+getYear();
+GetDay();
 
-    function getMonth() {
-        for (i = 0; i < monthNames.length; i++) {
-            text += "<option>" + monthNames[i] + "</option>";
-        }
-        document.getElementById("month").innerHTML = text;
+function getMonth() {
+    for (i = 0; i < monthNames.length; i++) {
+        month += "<option value=" + [i + 1] + ">" + monthNames[i] + "</option>";
+        // console.log(selectedMonth);
     }
+    document.getElementById("month").innerHTML = month;
+}
 
-    function getYear() {
-        var start = nowYear;
-        var end = '1900';
-        var options = "";
-        for (var year = start; year >= end; year--) {
-            options += "<option>" + year + "</option>";
-        }
-        document.getElementById("year").innerHTML = options;
-
+function getYear() {
+    var start = nowYear;
+    var end = '1900';
+    var myYear = "";
+    for (var year = start; year >= end; year--) {
+        myYear += "<option value=" + year + ">" + year + "</option>";
     }
+    document.getElementById("year").innerHTML = myYear;
+}
 
-    function daysInMonth(month, year) {
-        return new Date(year, month, 0).getDate();
-    }
+function daysInMonth(month, year) {
+    return new Date(year, month, 0).getDate();
+}
 
-
-    console.log(daysInMonth(10, 2013));
-    var days = daysInMonth(10, 2013);
-
+function GetDay() {
+    selectedYear = document.getElementById("year").value;
+    console.log(selectedYear);
+    selectedMonth = document.getElementById("month").value;
+    console.log(selectedMonth);
+    var days = daysInMonth(selectedMonth, selectedYear);
+    console.log(days);
+    var foo = new Array(days);//create a day element array
+    console.log(foo);
     var somex = "";
 
-    function getDays() {
-        for (i = 1; i < days; i++) {
-            somex += "<option>" + days[i] + "</option>";
-            // text +=  monthNames[i];
-        }
+    for (var i = 0; i < foo.length; i++) {
+        // console.log( (i+1) );
+        somex += "<option>" + (i + 1) + "</option>";
         document.getElementById("day").innerHTML = somex;
-
     }
+}
 
 
 // to get my date
 
-    // var selectedYear = document.getElementById("year").value;
-    // var selectedMonth = document.getElementById("month").value;
-    // console.log(getDaysInMonth(4, 2012));
+// var selectedYear = document.getElementById("year").value;
+// var selectedMonth = document.getElementById("month").value;
+// console.log(getDaysInMonth(4, 2012));
 
-    // var a = moment('2013-01-01');
-    // var b = moment('2013-06-01');
+// var a = moment('2013-01-01');
+// var b = moment('2013-06-01');
 
 // // If you want an exclusive end date (half-open interval)
 //     for (var m = moment(a); m.isBefore(b); m.add(1, 'days')) {
@@ -66,6 +68,7 @@
 //     for (var m = moment(a); m.diff(b, 'days') <= 0; m.add(1, 'days')) {
 //         console.log(m.format('YYYY-MM-DD'));
 //     }
-})();
 
-
+//
+// // moment().format('MMMM Do YYYY, h:mm:ss a'); // August 24th 2017, 1:47:58 pm
+// var nowMonth = moment().format('MMMM');
